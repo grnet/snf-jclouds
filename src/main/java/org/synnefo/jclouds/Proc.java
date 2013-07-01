cabcf4e295e24286a8ed93ba36e2d2abc2b199d0
Christos KK Loverdos <loverdos@gmail.com>
2013-07-01 12:55:59 +0300

    Add forgotten Proc<T>

---
+++ src/main/java/org/synnefo/jclouds/Proc.java
@@ -0,0 +1,19 @@
+package org.synnefo.jclouds;
+
+/**
+ * @author Christos KK Loverdos <loverdos@gmail.com>
+ */
+public interface Proc<T> {
+    public void apply(T value);
+
+    public static final class Helpers {
+        public static <T> Proc<T> safe(Proc<T> proc) {
+            if(proc != null) { return proc; }
+
+            return new Proc<T>() {
+                @Override
+                public void apply(T value) { }
+            };
+        }
+    }
+}
